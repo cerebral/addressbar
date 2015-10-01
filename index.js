e124dee4417044ce882b719a6b0fe3f5d678adba
christianalfoni <christianalfoni@gmail.com>
2015-10-01 21:50:59 +0200

    added IE9 fix

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 var EventEmitter = require('events').EventEmitter;
 var instance = null;
+var location = window.history.location || window.location;
+
+if (!location.origin) {
+  location.origin = location.protocol + "//" + location.hostname + (location.port ? ':' + location.port: '');
+}
+
 module.exports = (function () {
 
   if (instance) {
